6f02fd4d898ee0ad0287ba3f4b31435689390a50
Luis Vera <luisvera2318@gmail.com>
2022-06-15 22:41:52 -0500

    actividad switch

--- src/App.js
+++ src/App.js
@@ -2,36 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title';
 import FullName from './components/FullName';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [state, setCount] = useState(0);
+	const text = state === 0 ? "Off": "On";
 
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-
 				<Title text="Hola"></Title>
 				<Title text="Prueba2"></Title>
-
 				<FullName firstName="Luis" lastName="Vera"></FullName>
 
-				<p>Contador: {count}</p>
-				<button onClick={ () => { setCount(count + 1); }}> Incrementar </button>
-				<button onClick={ () => {	setCount(count - 1); }}> Decrementar </button>
+				<div className={"square " + text}>{text}</div>
+				<button onClick={ () => { setCount(1) }}> Encender </button>
+				<button onClick={ () => {	setCount(0) }}> Apagar </button>
+
 			</header>
 		</div>
 	);
