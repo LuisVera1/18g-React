f2a8c2dcb5f839438a842fb7eabe96cd11063bc7
Luis Vera <luisvera2318@gmail.com>
2022-06-17 20:20:45 -0500

    add changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useState, useEffect } from 'react';
 import React from 'react';
 
 function App() {
-	const [stateP, setItem] = useState(0);
+	const [stateP, setItem] = useState(null);
 	const [state, setIsOn] = useState(0);
 	const text = state === 0 ? "Off": "On";
 
@@ -24,11 +24,11 @@ function App() {
 				})}
 				className={"square " + text}>{text}</div>
 
-				<p className={stateP === 0? "selected": ""} onMouseOver={()=>setItem(0)}>Getting Started</p>
-				<p className={stateP === 1? "selected": ""} onMouseOver={()=>setItem(1)}>Add React to a Website</p>
-				<p className={stateP === 2? "selected": ""} onMouseOver={()=>setItem(2)}>Create a New React App</p>
-				<p className={stateP === 3? "selected": ""} onMouseOver={()=>setItem(3)}>CDN Links</p>
-				<p className={stateP === 4? "selected": ""} onMouseOver={()=>setItem(4)}>Release Channels</p>
+				<p className={stateP === 0? "selected": ""} onClick={()=>setItem(0)}>Getting Started</p>
+				<p className={stateP === 1? "selected": ""} onClick={()=>setItem(1)}>Add React to a Website</p>
+				<p className={stateP === 2? "selected": ""} onClick={()=>setItem(2)}>Create a New React App</p>
+				<p className={stateP === 3? "selected": ""} onClick={()=>setItem(3)}>CDN Links</p>
+				<p className={stateP === 4? "selected": ""} onClick={()=>setItem(4)}>Release Channels</p>
 
 
 
