0966d0ac7af03514b43f40160340c5f2f34f3c15
Luis Vera <luisvera2318@gmail.com>
2022-06-21 20:43:39 -0500

    conversor de divisas

--- src/App.js
+++ src/App.js
@@ -5,57 +5,29 @@ import { useState, useEffect } from 'react';
 import React from 'react';
 
 function App() {
-	const [stateP, setItem] = useState(null);
-	const [state, setIsOn] = useState(0);
+    const [amount, setAmount] = useState(null);
+  
+    const handleChangeAmount = ({ target: { value } }) => {
+      const newAmount = Number(value);
+      setAmount(newAmount);
+    };
 
-	const koders = [
-    {
-      firstName: "Luis",
-      lastName: "Vera",
-      age: 24,
-      gender: "m",
-      photoURL: "https://www.w3schools.com/howto/img_avatar.png",
-    },
-    {
-      firstName: "Nestor",
-      lastName: "Ramírez",
-      age: 40,
-      gender: "m",
-      photoURL: "https://www.w3schools.com/w3images/avatar2.png",
-    },
-    {
-      firstName: "David",
-      lastName: "Romero",
-      age: 28,
-      gender: "m",
-      photoURL: "https://www.w3schools.com/howto/img_avatar.png",
-    },
-    {
-      firstName: "Yusef",
-      lastName: "Lopéz",
-      age: 40,
-      gender: "m",
-      photoURL: "https://www.w3schools.com/w3images/avatar2.png",
-    },
-  ];
 
-  const kodersUIC = koders.map(
-    ({ firstName, lastName, age, gender, photoURL }, index) => (
-      <Card
-        key={index}
-        photoURL={photoURL}
-        firstName={firstName}
-        lastName={lastName}
-        age={age}
-        gender={gender}
-      />
-    )
-  );
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <ul className="list">{kodersUIC}</ul>
+
+      <div>
+        <p className="title">
+          La cantidad en USD es ${!amount ? "0.00" : (amount * 0.05).toFixed(2)}
+        </p>
+        <input type="number" value={amount} onChange={handleChangeAmount} />
+      </div>
+
+
+
       </header>
     </div>
   );
