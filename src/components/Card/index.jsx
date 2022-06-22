973de31a0d6efd8aa82d64b04eedcea66cf28d0c
Luis Vera <luisvera2318@gmail.com>
2022-06-21 19:34:59 -0500

    add cardItemContent

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,22 @@
+// Component
+import CardItemContent from "../CardItemContent";
 import "./Card.css";
 
 export default function Card({ photoURL, firstName, lastName, age, gender }) {
   return (
-    <li className="listStyle">
-      <div className="flexcontainer">
-        <div className="img">
-          <img className="imgThumb" src={photoURL} alt=""></img>
-        </div>
-        <div className="textContainer">
-          <p className="textBlack">{firstName + " " + lastName}</p>
-          <p className="textGray">{"Edad: " + age + ", Género: " + gender}</p>
-        </div>
+    <div className="card-container">
+      <div className="card-image-container">
+        <img alt="profile" src={photoURL} className="card-image" />
       </div>
-    </li>
+      <div className="card-content">
+        {/* Crear un componente llamado CardItemContent */}
+        <CardItemContent title={"Name"} content={`${firstName} ${lastName}`} />
+        <CardItemContent title={"Edad"} content={age} />
+        <CardItemContent
+          title={"Gender"}
+          content={gender === "m" ? "Masculino" : "Femenino"}
+        />
+      </div>
+    </div>
   );
 }
