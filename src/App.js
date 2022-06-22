182807afd4cc9059e8f603d333d86e6462f077c4
Luis Vera <luisvera2318@gmail.com>
2022-06-22 18:04:50 -0500

    conversor de unidades USD MXN

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-import Card from './components/Card/index'
 import { useState, useEffect } from 'react';
 import React from 'react';
 
-function App() {
-    const [amount, setAmount] = useState(null);
-  
-    const handleChangeAmount = ({ target: { value } }) => {
-      const newAmount = Number(value);
-      setAmount(newAmount);
-    };
+export default function App() {
+  const [mxn, setMXN] = useState(null);
+  const [usd, setUSD] = useState(null);
 
-
-
+  const handleChangeAmount = ({ target: { value } },currency) => {
+    if(currency === "mxn"){
+      setMXN(value)
+      setUSD((mxn * 0.05).toFixed(2))
+    } else{
+      setUSD(value)
+      setMXN((usd * 20.13).toFixed(2))
+    }
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-
-      <div>
-        <p className="title">
-          La cantidad en USD es ${!amount ? "0.00" : (amount * 0.05).toFixed(2)}
-        </p>
-        <input type="number" value={amount} onChange={handleChangeAmount} />
-      </div>
-
-
-
-      </header>
+      <p>MXN</p>
+      <input type="number" 
+        value={mxn}
+        onChange={(e)=>handleChangeAmount(e,"mxn")} />
+      <p>USD</p>
+      <input type="number"
+        value={usd}
+        onChange={(e) =>handleChangeAmount(e,"usd")} />
     </div>
   );
 }
-
-export default App;
