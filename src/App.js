4521a0c3739d60f47c25eb286f8402bc44737a79
Luis Vera <luisvera2318@gmail.com>
2022-06-23 20:15:41 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
+import * as React from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
-import { useState, useEffect } from 'react';
-import React from 'react';
+import About from "./pages/About/index"
 
 export default function App() {
-  const [mxn, setMXN] = useState(null);
-  const [usd, setUSD] = useState(null);
-
-  const handleChangeAmount = ({ target: { value } },currency) => {
-    if(currency === "mxn"){
-      setMXN(value)
-      setUSD((mxn * 0.05).toFixed(2))
-    } else{
-      setUSD(value)
-      setMXN((usd * 20.13).toFixed(2))
-    }
-  };
 
   return (
     <div className="App">
-      <p>MXN</p>
-      <input type="number" 
-        value={mxn}
-        onChange={(e)=>handleChangeAmount(e,"mxn")} />
-      <p>USD</p>
-      <input type="number"
-        value={usd}
-        onChange={(e) =>handleChangeAmount(e,"usd")} />
+      <Routes>
+        <Route path="/" element={<p>Estoy en el dashboard</p>}/>
+        <Route path="about" element={<About />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/About/index.jsx
@@ -0,0 +1,6 @@
+export default function About({}) {
+  return (
+    <p>Soy un about</p>
+  );
+}
+
